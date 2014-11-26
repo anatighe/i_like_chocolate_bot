129bd347d883fd59b6dcf732987f11ad3494003c
Ana Tighe <anadances@gmail.com>
2014-11-26 01:29:54 -0800

    reformatting login

--- chocolateBot.py
+++ chocolateBot.py
@@ -1,16 +1,28 @@
 import time
 import praw
+import pprint
 
-user_agent = ("Chocolate 1.0 by /u/beardgoggles")
+r = praw.Reddit("Chocolate 1.0 by /u/beardgoggles")
 
-r = praw.Reddit(user_agent=user_agent)
+username = input("username: ")
+password = input("Password: ")
 
-r.login()
+r.login(username,password)
+
 already_done = []
 
-prawWords = ['ganache', 'chocolate']
+prawWords = ['chocolate', 'ganache']
 while True:
     subreddit = r.get_subreddit('Baking')
+    for submission in subreddit.get_hot(limit=20):
+        op_text = submission.title.lower()
+        has_praw = any(string in op_text for string in prawWords)
+        if submission.id not in already_done and has_praw:
+            submission.upvote()
+            already_done.append(submission.id)
+            pprint.pprint(submission.title)
+            
+    time.sleep(30)
     
 
 
